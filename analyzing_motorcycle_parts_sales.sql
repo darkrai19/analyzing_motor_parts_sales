aab0ebaa49bfb52522e3dd46c15ec36df372080d
raimondelo <49273176+darkrai19@users.noreply.github.com>
2025-02-15 14:54:26 +0800

    Add files via upload

---
+++ analyzing_motorcycle_parts_sales.sql
@@ -0,0 +1,9 @@
+SELECT
+	product_line,
+	TO_CHAR(date, 'Month') AS month,
+	warehouse,
+	(SUM(total) - SUM(payment_fee)) AS net_revenue
+FROM sales
+WHERE client_type = 'Wholesale'
+GROUP BY product_line, month, warehouse
+ORDER BY product_line, month, net_revenue DESC
